a8983a9b05b22e78854acea46854ea9ebcdcb9a8
Jesse Monks <monksjesse@gmail.com>
2021-12-21 10:13:17 -0500

    improved logic and folder structure being created

--- generator.js
+++ generator.js
@@ -1,72 +1,103 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require('path');
 
 
 function init() {
-    defaultRouting();
+  defaultRouting();
 };
 
 
 function defaultRouting() {
 
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                message: 'How would you like to start?',
-                choices: ['Blank template', 'I would like to customize my template'],
-                name: 'defaultYN',
-            },
-        ])
-        .then((answers) => {
-            if (answers.defaultYN === 'Default no inputs please') return answers;
-            customize();
-        });
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: 'How would you like to start?',
+        choices: ['Blank template', 'I would like to customize my template'],
+        name: 'defaultYN',
+      },
+    ])
+    .then((answers) => {
+      if (answers.defaultYN === 'Default no inputs please') return answers;
+      customize();
+    });
 }
 
 function customize() {
 
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: 'What is the name of the module?',
-                name: 'moduleName',
-            },
-            {
-                type: 'list',
-                message: 'What language will you be coding in?',
-                choices: ['javascript', 'lua' , 'C#'],
-                name: 'language',
-            },
-            {
-                type: 'confirm',
-                message: 'Are you creating a UI?',
-                name: 'html',
-            },
-            {
-                type: 'confirm',
-                message: 'Are you writing any Javascript?',
-                name: 'js',
-            },
-            {
-                type: 'confirm',
-                message: 'Will you need need a .css file?',
-                name: 'css',
-                when(answers) {
-                    return answers.html === true;
-                }
-            },
-            {
-                type: 'input',
-                message: 'What is the name of the module?',
-                name: 'moduleName',
-            }
-        ])
-        .then((answers) => {
-            console.log('set 2', answers)
-        });
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'What is the name of the module?',
+        name: 'moduleName',
+      },
+      {
+        type: 'list',
+        message: 'What language will you be coding in?',
+        choices: ['javascript', 'lua', 'C#'],
+        name: 'language',
+      },
+      {
+        type: 'confirm',
+        message: 'Are you creating a UI?',
+        name: 'html',
+      },
+      {
+        type: 'confirm',
+        message: 'Are you writing any Javascript?',
+        name: 'js',
+      },
+      {
+        type: 'confirm',
+        message: 'Will you need need a .css file?',
+        name: 'css',
+        when(answers) {
+          return answers.html === true;
+        }
+      },
+      {
+        type: 'input',
+        message: 'What is the name of the module?',
+        name: 'moduleName',
+      }
+    ])
+    .then((answers) => {
+      if (answers.html) makeDirectoryPath(answers);
+    })
 }
+
+
+
+function makeDirectoryPath(answers) {
+  const { html, js, css } = answers;
+  fs.mkdir(path.join(__dirname, `fivem-${answers.moduleName}`), (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('Directory created successfully!');
+  });
+  makeFiles(html, js, css);
+};
+
+function makeFiles(html, js, css) {
+
+  for (let i = 0; i < arguments.length; i++) {
+    if (i) console.log('tesst')
+  }
+
+
+
+  /* fs.writeFile(path.join(__dirname, `test.js`), (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('File created successfully!');
+  }); */
+}
+
 
 
 
