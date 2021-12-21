4e5b900ba497101b17d551c417d25450dcc3c31e
Jesse <monksjesse@gmail.com>
2021-12-21 00:54:16 -0500

    logic started, helper functions implemented

--- generator.js
+++ generator.js
@@ -1,14 +1,73 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: '',
-      name: '',
-    }
-  ])
-  .then((answers) => {
-    console.log(answers)
-  });
+
+function init() {
+    defaultRouting();
+};
+
+
+function defaultRouting() {
+
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'How would you like to start?',
+                choices: ['Blank template', 'I would like to customize my template'],
+                name: 'defaultYN',
+            },
+        ])
+        .then((answers) => {
+            if (answers.defaultYN === 'Default no inputs please') return answers;
+            customize();
+        });
+}
+
+function customize() {
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the name of the module?',
+                name: 'moduleName',
+            },
+            {
+                type: 'list',
+                message: 'What language will you be coding in?',
+                choices: ['javascript', 'lua' , 'C#'],
+                name: 'language',
+            },
+            {
+                type: 'confirm',
+                message: 'Are you creating a UI?',
+                name: 'html',
+            },
+            {
+                type: 'confirm',
+                message: 'Are you writing any Javascript?',
+                name: 'js',
+            },
+            {
+                type: 'confirm',
+                message: 'Will you need need a .css file?',
+                name: 'css',
+                when(answers) {
+                    return answers.html === true;
+                }
+            },
+            {
+                type: 'input',
+                message: 'What is the name of the module?',
+                name: 'moduleName',
+            }
+        ])
+        .then((answers) => {
+            console.log('set 2', answers)
+        });
+}
+
+
+
+init();
---
+++ helpers/templates.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+
+function htmlTemplate() {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
+    </html>`
+}
+
+function cssTemplate() {
+    return fs.writeFile()
+
+}
+
+function jsTepmplate() {
+
+}
+
+function luaTemplate() {
+
+}
+
+function cSharpTemplate() {
+
+}
